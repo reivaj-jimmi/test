b035f5f8254124d31e2ce4bccce86baad256c2c8
reivaj-jimmi <jimmiarguellozaracho@gmail.com>
2025-03-31 18:35:21 -0400

    Inicio de Proyecto

---
+++ 05_intro_js/js/app.js
@@ -0,0 +1,52 @@
+// //par o impar
+// let numero=parseInt (prompt("Ingrese un numero"));
+// //Esto es para declarar una variable
+// console.log(typeof numero);
+
+// if (numero%2==0){
+//     alert("El numero ingresado es par");
+//     console.log("El numero "+numero+" es par")
+// }
+// else{alert("EL numero es impar")
+//     console.log("El numero "+numero+" es impar")
+// }
+// let numero1 =parseInt (prompt("Ingrese un numero"))
+// let numero2 =parseInt (prompt("Ingrese el segundo numero"))
+
+// if(numero1>numero2){
+//     alert("EL primer numero ingresado es el mayor")
+// }
+// else{
+//     alert("EL segundo numero ingresado es el mayor")
+// }
+
+// for (let index = 0; index < 101; index++) {
+//         if (index%2==0) {
+//             console.log(index);
+//         }
+   
+//     ;   
+// }
+// let numero1=parseInt(prompt("Ingrea un numero"))
+// let numero2=parseInt(prompt("Ingrese el segundo numero"))
+// if (numero1==numero2) {
+//     console.log("Los dos numeros tienen que ser diferentes");
+    
+//     }
+//     let mayor=0;
+//     let menor=0;
+// if(numero1>numero2){
+//     mayor=numero1;
+//     menor=numero2;
+// }else{
+//     mayor=numero2;
+//     menor=numero1;
+// }
+// for (let x= menor; x< mayor; x++) {
+//    console.log(x);
+   
+    
+// }
+    
+
+    
---
+++ 05_intro_js/js/funciones.js
@@ -0,0 +1,10 @@
+let numero1=parseInt(prompt("Ingresa un valor"))
+let numero2=parseInt(prompt("Ingresa el segundo valor"))
+let numero3=parseInt(prompt("Ingresa el tercer  valor"))
+function promedio(numero1,numero2,numero3){
+    let promedio2=(numero1+numero2+numero3)/3
+    return promedio2;
+    
+}
+
+console.log(promedio(numero1, numero2, numero3));
